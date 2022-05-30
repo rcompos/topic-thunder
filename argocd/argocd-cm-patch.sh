b3d1e65b41084537b779a9b699b21066c7240203
Ron Compos <rcompos@gmail.com>
2022-05-29 22:48:50 -0600

    proj-topic-thunder Add icon.

--- argocd/argocd-cm-patch.sh
+++ argocd/argocd-cm-patch.sh
@@ -2,12 +2,15 @@
 set +x
 
 #    "ui.cssurl": "./custom/my-styles.css",
+#    "ui.cssurl": "http://topic-thunder.scandalizer.org/my-styles.css",
+#    "ui.cssurl": "http://topic-thunder.scandalizer.org.s3-website.us-west-2.amazonaws.com/my-styles.css",
+#    "ui.cssurl": "https://rcompos.github.io/my-styles.css",
 
 kubectl -n argocd patch configmap/argocd-cm --type merge \
   -p '{ "data":{
-    "ui.cssurl": "http://topic-thunder.scandalizer.org/my-styles.css",
+    "ui.cssurl": "https://rcompos.github.io/my-styles.css",
     "ui.bannercontent": "Hello ArgoCD afficianado!",
-    "ui.bannerurl": "blacklab.lan"
+    "ui.bannerurl": "https://github.com/rcompos/topic-thunder"
  }}'
 
 
